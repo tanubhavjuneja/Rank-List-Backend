ce09c8abf61f48908cbd04d7256c4c445a3e73f6
souravvv2004 <121968251+souravvv2004@users.noreply.github.com>
2025-05-23 20:22:23 +0530

    Update server.py

--- server.py
+++ server.py
@@ -73,4 +73,5 @@ def get_ranks():
         print("DB error:", e)
         return jsonify({"error": "Database error"}), 500
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=11540, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
